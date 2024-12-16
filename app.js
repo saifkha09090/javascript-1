d8dcf0c80de9d5193aa4461c5db7835fae3376fe
Saifullah Khan <saifullahkha09090@gmail.com>
2024-12-16 20:39:56 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,119 @@
+// Chapter no 1
+
+// Question no 1
+
+// alert("welcome to my website");
+
+// Question no 2
+
+// alert("Error! Please enter a valid password.");
+
+// Question no 3
+
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+// Question no 4
+
+// alert("Welcome to JS Land... ");
+
+// alert("Happy Coding!");
+
+// Question no 5
+
+// alert("Hello... I can run JS through my web browser's console");
+
+// Question no 6
+
+// alert("hello world")
+
+// Question no 7 answer in html file
+
+// Chapter no 1 Complete
+
+// Chapter no 2
+
+// Question no 1
+
+// let username;
+
+// Question no 2
+
+// let myName = "Saifullah khan";
+
+// Question no 3
+
+// let message = "Hello world";
+// alert(message);
+
+// Question no 4
+
+// let studentName = "Saifullah khan";
+// let age = "20 years old";
+// let course = "Web Developent";
+
+// alert(studentName);
+// alert(age);
+// alert(course);
+
+// Question no 5
+
+// let food = " Pizza";
+
+// alert(food + "\n Pizz \n Piz \n Pi \n P");
+
+// Question no 6
+
+// let email = "saifullahkha09090@gmail.com";
+
+// alert("My email address is " + email);
+
+// Question no 7
+
+// let book = "A smarter way to learn JavaScript.";
+
+// alert("I am trying to learn from the Book " + book);
+
+// Question no 8
+
+// document.write("<h2>Yah! I can write HTML content through JavaScript</h2>");
+
+// Question no 9
+
+// let sign = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// alert(sign);
+
+// Chapter no 2 Complete
+
+
+// Chapter no 3
+
+
+// Question no 1
+
+// let age = 20;
+
+// alert("I am " + age + " years old");
+
+// Question no 2
+
+// let visitCount = 0;
+// visitCount++;
+
+// alert("You have visited this website " + visitCount + " times");
+
+// Question no 3
+
+// let birthYear = 2004;
+
+// document.write(`<p>My birth year is ${birthYear}</p>  <p>Data type of my declared variable is number</p>`);
+
+// Question no 4
+
+// let visitorName = "Saifullah";
+// let productTitle = " bike";
+// let quantity = 5;
+
+// document.write(`<p>${visitorName} ordered ${quantity + productTitle}'s on xyz store. </p>`);
+
+// Chapter no 3 Complete
